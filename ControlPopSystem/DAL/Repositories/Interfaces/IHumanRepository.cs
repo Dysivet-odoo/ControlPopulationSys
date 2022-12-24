42aaadedcd3c5ffbe98069eb898d76f2e337b9f2
Dysivet <100676315+Dysivet@users.noreply.github.com>
2022-12-24 09:53:14 +0200

    Added Contracts of Repo
    Added Contracts of Repo

---
+++ ControlPopSystem/DAL/Repositories/Interfaces/IHumanRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DALL.Entities;
+
+namespace DALL.Repositories.Interfaces
+{
+    public interface IHumanRepository
+        :IRepository<Human>
+    {
+    }
+}
