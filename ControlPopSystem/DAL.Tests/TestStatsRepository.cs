8ad5c738b0760c7e5c08f7bef5f47b06a974d0f5
Dysivet <100676315+Dysivet@users.noreply.github.com>
2022-12-24 10:21:33 +0200

    Addded Sol
    ddd

---
+++ ControlPopSystem/DAL.Tests/TestStatsRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PopControl.DAL.Entities;
+using PopControl.DAL.Repositories.Impl;
+using Microsoft.EntityFrameWorkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Tests
+{
+    public class TestStatsRepository
+        : BaseRepository<Statistics>
+    {
+        public TestStatsRepository(DbContext context) 
+            : base(context)
+        {
+        }
+    }
+}
